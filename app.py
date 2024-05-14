0d3e99afea306d0823e821bc282283f4b35dcf83
Manu Mohan <manu.m@thinkpalm.com>
2024-05-14 12:50:59 +0530

    Updates:
    -port set to 80

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def upload_file():
         return redirect(request.url)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
